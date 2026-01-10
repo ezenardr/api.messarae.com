5daa008241026a92caeeb6465e790b27b22a8454
ezenard <ezenardr.dev@gmail.com>
2026-01-10 15:29:01 -0500

    Feat: add dockerfile

--- start/routes.ts
+++ start/routes.ts
@@ -6,4 +6,12 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+
+import router from '@adonisjs/core/services/router'
 import './routes/auth.js'
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
