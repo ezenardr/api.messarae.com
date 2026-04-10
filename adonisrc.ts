6f0710e30941353f1d35d1cfe84a37514c4920d2
ezenardr <ezenardr.dev@gmail.com>
2026-04-10 18:39:03 -0400

    Update: email template

--- adonisrc.ts
+++ adonisrc.ts
@@ -1,30 +1,19 @@
 import { defineConfig } from '@adonisjs/core/app'
 
 export default defineConfig({
-  /*
-  |--------------------------------------------------------------------------
-  | Experimental flags
-  |--------------------------------------------------------------------------
-  |
-  | The following features will be enabled by default in the next major release
-  | of AdonisJS. You can opt into them today to avoid any breaking changes
-  | during upgrade.
-  |
-  */
   experimental: {
     mergeMultipartFieldsAndFiles: true,
     shutdownInReverseOrder: true,
   },
 
-  /*
-  |--------------------------------------------------------------------------
-  | Commands
-  |--------------------------------------------------------------------------
-  |
-  | List of ace commands to register from packages. The application commands
-  | will be scanned automatically from the "./commands" directory.
-  |
-  */
+  // 👇 Add this
+  metaFiles: [
+    {
+      pattern: 'resources/views/**/*.edge',
+      reloadServer: false,
+    },
+  ],
+
   commands: [
     () => import('@adonisjs/core/commands'),
     () => import('@adonisjs/lucid/commands'),
@@ -32,15 +21,6 @@ export default defineConfig({
     () => import('@adonisjs/mail/commands'),
   ],
 
-  /*
-  |--------------------------------------------------------------------------
-  | Service providers
-  |--------------------------------------------------------------------------
-  |
-  | List of service providers to import and register when booting the
-  | application
-  |
-  */
   providers: [
     () => import('@adonisjs/core/providers/app_provider'),
     () => import('@adonisjs/core/providers/hash_provider'),
@@ -57,25 +37,8 @@ export default defineConfig({
     () => import('@adonisjs/core/providers/edge_provider'),
   ],
 
-  /*
-  |--------------------------------------------------------------------------
-  | Preloads
-  |--------------------------------------------------------------------------
-  |
-  | List of modules to import before starting the application.
-  |
-  */
   preloads: [() => import('#start/routes'), () => import('#start/kernel')],
 
-  /*
-  |--------------------------------------------------------------------------
-  | Tests
-  |--------------------------------------------------------------------------
-  |
-  | List of test suites to organize tests by their type. Feel free to remove
-  | and add additional suites.
-  |
-  */
   tests: {
     suites: [
       {
